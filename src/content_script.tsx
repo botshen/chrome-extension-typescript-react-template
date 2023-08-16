8f3b147aa5090af6a660252bc15f789e6b262217
shenxin <botzgbj@gmail.com>
2023-08-17 00:24:32 +0800

    add inject

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -24,3 +24,12 @@
 import { changeBackgroundListener } from "./message_passing"
 
 chrome.runtime.onMessage.addListener(changeBackgroundListener)
+const script = document.createElement('script')
+script.setAttribute('type', 'text/javascript')
+script.setAttribute('src', chrome.runtime.getURL('js/inject.js'))
+document.documentElement.appendChild(script)
+
+const input = document.createElement('input')
+input.setAttribute('id', 'ajaxInterceptor')
+input.setAttribute('style', 'display:none')
+document.documentElement.appendChild(input)
